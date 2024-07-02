cf2263c432e210702836e36dbc50be42c9c695a4
YeezyG <905004540@qq.com>
2024-07-02 12:01:57 +0800

    测试自动部署

---
+++ src/router/router.ts
@@ -0,0 +1,21 @@
+import { createWebHistory, createRouter } from "vue-router";
+import { RouteRecordRaw } from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: "/",
+        name: "home",
+        component: () => import('../components/Home/home.vue')
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+router.afterEach(() => {
+    window.scrollTo(0, 0)
+})
+
+export default router;
