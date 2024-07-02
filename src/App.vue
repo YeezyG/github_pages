24b90dd6307f491f798c29ea367d00bb367a168b
YeezyG <905004540@qq.com>
2024-07-02 16:27:16 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const readyRender = ref<boolean>(false)
-
-inital()
-
-function inital() {
-  readyRender.value = true
-}
 </script>
 
 <template>
-  <div v-if="readyRender">
+  <div>
     <router-view></router-view>
   </div>
 </template>
