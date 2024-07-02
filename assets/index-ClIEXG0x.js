9cf6b76298cced290b6ddb24393302d448fb6ae5
YeezyG <149983588+YeezyG@users.noreply.github.com>
2024-07-02 08:05:17 +0000

    Deploying to gh-pages from @ YeezyG/github_pages@5749b96f30a490b12fc16f43f02ec3416ca503d6 🚀

---
+++ assets/index-ClIEXG0x.js
@@ -0,0 +1,1 @@
+import{d as n,r,c as s,a as o,b as c,e as _,f as i,o as d,T as p}from"./index-DJwo3PWz.js";const m={key:0},u=_("div",{style:{"margin-top":"200px"}},"测试",-1),B=n({__name:"index",setup(l){const e=r(!1);t();function t(){e.value=!0}return(f,v)=>{const a=i("router-view");return e.value?(d(),s("div",m,[o(p),u,o(a)])):c("",!0)}}});export{B as default};
