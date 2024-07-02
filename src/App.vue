475e0c225383d5b9eabc91bec0d2df484d2cc12b
YeezyG <905004540@qq.com>
2024-07-02 15:46:14 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import TopNavBar from './components/modules/TopNavBar.vue'
 </script>
 
 <template>
   <div>
     <div>测试</div>
+    <TopNavBar></TopNavBar>
     <router-view></router-view>
   </div>
 </template>
